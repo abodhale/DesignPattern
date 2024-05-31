6f85da046bb26a5f2fce37a3cb52a23088c69f8f
abodhale <abodhale@myyahoo.com>
2024-05-31 17:38:21 -0500

    Update resource.java

--- Builder Pattern/resource.java
+++ Builder Pattern/resource.java
@@ -8,15 +8,14 @@ public class resource
 		//Databaseconnection conn =Databaseconnection.getInstance();
 		
 		PaymentBuilder payment1 = new PaymentBuilder.Builder()
-				.setAmount(0)
-				.setCurrency("USD")
-                .setPaymentMethod("Credit Card")
-                .setRecipient("John Doe")
-                .setTimestamp(LocalDateTime.now())
-                .setDescription("Payment for services")
-                .build();
-
-        System.out.println(payment1);
+							    .setAmount(0)
+							    .setCurrency("USD")
+                  				            .setPaymentMethod("Credit Card")
+                                                            .setRecipient("John Doe")
+                				            .setTimestamp(LocalDateTime.now())
+                                                            .setDescription("Payment for services")
+                                                            .build();
+           System.out.println(payment1);
 
 	}
 
