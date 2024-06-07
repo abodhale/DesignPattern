2d93034b0988b10208fa5e9b7e06233bb7d820d7
abodhale <abodhale@myyahoo.com>
2024-06-07 01:39:34 -0500

    Update BOFABank.java

--- Adapter/ThirdParty/BOFABank.java
+++ Adapter/ThirdParty/BOFABank.java
@@ -5,7 +5,5 @@ public class BOFABank {
 	public void transferMoney  (String from,String to,String amt) 
 	{
 		System.out.println("sending money from "+from+" to "+to +" amount "+amt+"uing BOFA bank.");
-	}
-	
-	
+	}	
 }
