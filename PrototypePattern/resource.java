f019fd2fb0d49f4e064aa3015bdbf8ec9314bbac
abodhale <abodhale@myyahoo.com>
2024-06-07 01:35:49 -0500

    Update resource.java

--- PrototypePattern/resource.java
+++ PrototypePattern/resource.java
@@ -4,22 +4,7 @@ public class resource
 {
 
 	public static void main(String[] args) 
-	{
-		//Databaseconnection conn =Databaseconnection.getInstance();
-		
-		//		PaymentBuilder payment1 = new PaymentBuilder.Builder()
-		//				.setAmount(0)
-		//				.setCurrency("USD")
-		//                .setPaymentMethod("Credit Card")
-		//                .setRecipient("John Doe")
-		//                .setTimestamp(LocalDateTime.now())
-		//                .setDescription("Payment for services")
-		//                .build();
-		//
-		//        System.out.println(payment1);
-
-		
-		
+	{	
 		DevConfiguration dev=new DevConfiguration("jdbc:mysql://localhost:3306/dev","http://api.example.com/dev","400 ms",true);
 		
 		DevConfiguration dev2 = dev.clone();
